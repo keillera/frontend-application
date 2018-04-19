1e867983cf43235af0f7c957693f72a7af5c68c5
keillera <keikaillera@gmail.com>
2018-04-19 18:05:36 +0900

    Add gtm plugin

---
+++ app/plugins/gtm.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+export default ({ app }) => {
+  /*
+  ** クライアントサイドかつプロダクションモードでのみ実行
+  */
+  // if (process.env.NODE_ENV !== 'production') return
+
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+      'gtm.start':
+        new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-5DZNDNB');
+
+
+  app.router.afterEach((to, from) => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'gtm.js', 'gtm.start': new Date().getTime()
+    });
+    const currentUser = localStorage.getItem(
+      `CognitoIdentityServiceProvider.${process.env.CLIENT_ID}.LastAuthUser`
+    )
+    // console.log(currentUser)
+    // dataLayer.push({ 'event': 'pageview' })
+    dataLayer.push(to.gtm || { userId: currentUser })
+    // console.log(to.fullPath)
+    // dataLayer.push(to.gtm || { pageType: 'PageView', pageUrl: to.fullPath })
+  })
+}

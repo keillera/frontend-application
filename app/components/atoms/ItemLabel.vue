f004b4419f0f4da82ada26a180cc07e0102ff01e
keillera <keikaillera@gmail.com>
2021-05-14 04:22:06 +0900

    Add coin-ranking.

---
+++ app/components/atoms/ItemLabel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="label-area">
+    <div class="label">
+      {{ labelName }}
+    </div>
+    <nuxt-link v-if="moreLink !== null" :to="moreLink" class="more-link-pc">
+      もっと見る
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    labelName: String,
+    moreLink: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.label-area {
+  border-bottom: 1px solid rgba(#cecece, 0.3);
+  display: flex;
+  justify-content: space-between;
+}
+.label {
+  color: #333333;
+  font-size: 16px;
+  font-weight: bold;
+  border-bottom: 1px solid #6e6e6e;
+}
+.more-link-pc {
+  color: #6e6e6e;
+  font-size: 14px;
+  font-weight: bold;
+  text-decoration: none;
+}
+</style>
---
+++ app/components/organisms/CoinRankingTop.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="area-category-ranking-container">
+    <item-label
+      v-if="$route.query.topic === 'crypto'"
+      :labelName="rankingLabel"
+      :moreLink="moreLink"
+    />
+    <div class="coin-ranking">
+      <coin-ranking-list :indexCount="3" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemLabel from '../atoms/ItemLabel'
+import CoinRankingList from '../molecules/CoinRankingList'
+
+export default {
+  components: {
+    ItemLabel,
+    CoinRankingList
+  },
+  data() {
+    return {
+      rankingLabel: '時価総額ランキング',
+      moreLink: '/articles/crypto/coin_ranking'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.area-category-ranking-container {
+  grid-area: ranking;
+  margin: 30px 0 10px 0;
+}
+.coin-ranking {
+  margin: 25px 0 0 0;
+}
+</style>
